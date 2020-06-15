c9279ce268633f15f420074d03c530160041b657
douglastomizawa <tomizawadouglas@gmail.com>
2020-06-15 02:50:32 -0300

    Construindo a agenda

--- app/js/index.js
+++ app/js/index.js
@@ -1,14 +1,17 @@
 const range = require('../common/data/range');
 const jobsList = require('../common/data/jobsList');
 
-jobsList.sort((a, b) => new Date(a.maxDate) - new Date(b.maxDate))
-
-
 const scheduling = ({ range, jobsList }) => {
 
+    jobsList.sort((a, b) => new Date(a.maxDate) - new Date(b.maxDate))
+
     const days = new Array(new Date(range.finalDate).getDate() - new Date(range.startDate).getDate() + 1);
+
+    const schedule = [];
+
     console.log(days);
-     
+    console.log(schedule)
+
 }
 console.log(jobsList);
 module.exports = scheduling;
