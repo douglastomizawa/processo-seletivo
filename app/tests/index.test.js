83ef1a040158814531e82268c149b6b880c6b648
douglastomizawa <tomizawadouglas@gmail.com>
2020-06-15 01:49:15 -0300

    [TESTE] - Cada array deve conter jobs que sejam executados em no máximo 8h

--- app/tests/index.test.js
+++ app/tests/index.test.js
@@ -41,3 +41,62 @@ describe("- Cada array do conjunto representa uma lista de Jobs a serem executad
             .toEqual([[3, 2, 1], []]);
     });
 });
+
+
+describe("- Cada array deve conter jobs que sejam executados em no máximo 8h:", () => {
+    test("Quando a soma das horas de execução forem igual ou menor que 8, os jobs devem estar no mesmo array", () => {
+        expect(scheduling({
+            range,
+            jobsList: [
+                {
+                    'id': 1,
+                    'description': 'Importação de arquivos de fundos',
+                    'maxDate': '2019-11-10T12:00:00',
+                    'estimatedTime': 2,
+                },
+                {
+                    'id': 2,
+                    'description': 'Importação de dados da Base Legada',
+                    'maxDate': '2019-11-11T12:00:00',
+                    'estimatedTime': 2,
+                },
+                {
+                    'id': 3,
+                    'description': 'Importação de dados da Base Legada',
+                    'maxDate': '2019-11-12T12:00:00',
+                    'estimatedTime': 4,
+                }
+            ]
+        }))
+            .toEqual([[1, 2, 3], []]);
+    });
+    test("Quando a soma das horas de execução passarem de 8, o job deve retornar no próximo array", () => {
+        expect(scheduling({
+            range: {
+                'startDate': '2019-11-10T09:00:00',
+                'finalDate': '2019-11-12T12:00:00'
+            },
+            jobsList: [
+                {
+                    'id': 1,
+                    'description': 'Importação de arquivos de fundos',
+                    'maxDate': '2019-11-10T12:00:00',
+                    'estimatedTime': 5,
+                },
+                {
+                    'id': 2,
+                    'description': 'Importação de dados da Base Legada',
+                    'maxDate': '2019-11-11T12:00:00',
+                    'estimatedTime': 5,
+                },
+                {
+                    'id': 3,
+                    'description': 'Importação de dados da Base Legada',
+                    'maxDate': '2019-11-12T12:00:00',
+                    'estimatedTime': 5,
+                }
+            ]
+        }))
+            .toEqual([[1], [2], [3]]);
+    });
+});
