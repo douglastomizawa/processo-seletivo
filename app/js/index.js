7e944a26fafb3fe7c2f6f5111765f74c17152526
douglastomizawa <tomizawadouglas@gmail.com>
2020-06-15 02:59:07 -0300

    Construindo agenda

--- app/js/index.js
+++ app/js/index.js
@@ -2,13 +2,34 @@ const range = require('../common/data/range');
 const jobsList = require('../common/data/jobsList');
 
 const scheduling = ({ range, jobsList }) => {
-
     jobsList.sort((a, b) => new Date(a.maxDate) - new Date(b.maxDate))
-
-    const days = new Array(new Date(range.finalDate).getDate() - new Date(range.startDate).getDate() + 1);
-
+    const days = new Array(
+        new Date(range.finalDate).getDate() - new Date(range.startDate).getDate() + 1
+    );
+    
     const schedule = [];
 
+    for (let i = 0; i < days.length; i++) {
+        days[i] = new Array(24);
+        schedule.push({
+            hours: 0,
+            jobsList: new Array()
+        });
+    }
+
+    days[0][new Date(range.startDate).getHours()] = '-';
+    days[days.length - 1][new Date(range.finalDate).getHours()] = '-';
+    days.forEach((day) => {
+        for (hour = 0; hour < day.length; hour++) {
+            if (count != 0) {
+                day[hour] = 0;
+            } else if (day[hour] === '-') {
+                count++;
+            } else if (count >= 2) {
+                break;
+            }
+        }
+    })
     console.log(days);
     console.log(schedule)
 
