f4f8401d1c2cbe6cc1e831b8d2c82e648744a999
douglastomizawa <tomizawadouglas@gmail.com>
2020-06-15 01:35:03 -0300

    [TESTE] - Cada array do conjunto representa uma lista de Jobs a serem executados em sequência

--- app/tests/index.test.js
+++ app/tests/index.test.js
@@ -11,3 +11,33 @@ describe("- O output deve ser [[1, 3], [2]]", () => {
             .toEqual([[1, 3], [2]]);
     });
 });
+
+
+describe("- Cada array do conjunto representa uma lista de Jobs a serem executados em sequência:", () => {
+    test("Validar se os jobs estão ordenados pela data máxima de conclusão.", () => {
+        expect(scheduling({
+            range,
+            jobsList: [
+                {
+                    'id': 1,
+                    'description': 'Importação de arquivos de fundos',
+                    'maxDate': '2019-11-12T12:00:00',
+                    'estimatedTime': 2,
+                },
+                {
+                    'id': 2,
+                    'description': 'Importação de dados da Base Legada',
+                    'maxDate': '2019-11-11T12:00:00',
+                    'estimatedTime': 2,
+                },
+                {
+                    'id': 3,
+                    'description': 'Importação de dados da Base Legada',
+                    'maxDate': '2019-11-10T12:00:00',
+                    'estimatedTime': 2,
+                }
+            ]
+        }))
+            .toEqual([[3, 2, 1], []]);
+    });
+});
