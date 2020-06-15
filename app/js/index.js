303a0721aa31ca1237f19759ef0494e19b67364d
douglastomizawa <tomizawadouglas@gmail.com>
2020-06-15 02:43:31 -0300

    Início do desenvolvimento

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,14 @@
 const range = require('../common/data/range');
 const jobsList = require('../common/data/jobsList');
 
-const scheduling = ({ range, jobsList }) => {}
+jobsList.sort((a, b) => new Date(a.maxDate) - new Date(b.maxDate))
+
+
+const scheduling = ({ range, jobsList }) => {
+
+    const days = new Array(new Date(range.finalDate).getDate() - new Date(range.startDate).getDate() + 1);
+    console.log(days);
+     
+}
+console.log(jobsList);
 module.exports = scheduling;
